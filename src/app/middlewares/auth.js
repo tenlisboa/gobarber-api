fa673da3ce7a1503b4826d791361e419177db174
Gabriel Bento Lisboa <gabrielbento.rx@outlook.com>
2019-06-24 22:35:27 -0300

    Create auth middleware and validation rules

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
 import authConfig from '../../config/auth';
-import User from '../models/User';
 
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -16,12 +15,10 @@ export default async (req, res, next) => {
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    req.authUser = await User.findByPk(decoded.id);
+    req.userId = decoded.id;
 
-    next();
+    return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token malformed.' });
   }
-
-  next();
 };
