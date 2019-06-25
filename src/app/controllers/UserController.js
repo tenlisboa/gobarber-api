1950d1d1dc404d08cc58211cf9adf1fdbf6be5c8
Gabriel Bento Lisboa <gabrielbento.rx@outlook.com>
2019-06-24 22:07:48 -0300

    Initial configrations

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,23 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(422).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+
+  async update(req, res) {
+    return res.json({ status: true });
+  }
+}
+
+export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0efd7c99bf29fafbc2080632c9e13644',
+  espiresIn: '7d',
+};
