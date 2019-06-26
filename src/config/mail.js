98e49beb498cac01479e3560e17e755403d9f0fa
Gabriel Bento Lisboa <gabrielbento.rx@outlook.com>
2019-06-26 07:49:21 -0300

    Configure nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '91e973c30f0139',
+    pass: '3ef907a144d02d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
